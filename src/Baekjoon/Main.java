ef29030ed91354de7e917b48c6bff3ae992a614f
JangHyeonSuk <vkdnjdjxor@naver.com>
2023-03-27 01:04:44 +0900

    문자열 2675,1152,2908 문제 코드

--- src/Baekjoon/Main.java
+++ src/Baekjoon/Main.java
@@ -1,7 +1,10 @@
 package Baekjoon;
 
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
     }
 }
