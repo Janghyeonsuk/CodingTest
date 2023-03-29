4ce3c01760c38df27a4095f5be57d284402d7468
JangHyeonSuk <vkdnjdjxor@naver.com>
2023-03-29 20:10:03 +0900

    out file remove

--- src/Baekjoon/조건문/Main.java
+++ src/Baekjoon/조건문/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class num_1330 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
