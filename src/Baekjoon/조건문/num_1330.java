2850a72412d8a3c6e116208155fd548cf8931b34
JangHyeonSuk <vkdnjdjxor@naver.com>
2023-03-29 20:06:27 +0900

    commit 복구 및 입출력과 사칙연산 , 1330, 25083 recommit

---
+++ src/Baekjoon/조건문/num_1330.java
@@ -0,0 +1,23 @@
+package Baekjoon.조건문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A > B)
+            System.out.println(">");
+        else if (A == B)
+            System.out.println("==");
+        else
+            System.out.println("<");
+    }
+}
