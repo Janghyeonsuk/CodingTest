0fd282776370ee93477953c78b99485a646ca2b7
Hamsik <vkdnjdjxor@naver.com>
2024-04-27 00:56:46 +0900

    recursion boj1629 commit

---
+++ src/baekjoon/recursion/BOJ1629.java
@@ -0,0 +1,32 @@
+package baekjoon.recursion;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1629 {
+    static long a, b, c;
+
+    public static long pow(long a, long b, long c) {
+        if (b == 1)
+            return a % c;
+        long val = pow(a, b / 2, c);
+        val = val * a % c;
+        if (b % 2 == 0)
+            return val;
+
+        return val * a % c;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        System.out.println(pow(a, b, c));
+    }
+}
+
+
