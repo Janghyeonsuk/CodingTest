432efb6b7201291f119e8a6fcb513d7df8c09c92
Hamsik <vkdnjdjxor@naver.com>
2024-05-07 19:23:52 +0900

    the rest backtracking boj problem commit

--- src/baekjoon/backtracking/BOJ1182.java
+++ src/baekjoon/backtracking/BOJ1182.java
@@ -1,2 +1,35 @@
-package baekjoon.backtracking;public class BOJ1182 {
+package baekjoon.backtracking;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1182 {
+    static int N, S, cnt;
+    static int[] arr;
+
+    public static void solve(int k) {
+        if (k == S) {
+            cnt++;
+            return;
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        solve(0);
+        System.out.println(cnt);
+    }
 }
+
+
--- src/baekjoon/backtracking/BOJ9663.java
+++ src/baekjoon/backtracking/BOJ9663.java
@@ -1,2 +1,40 @@
-package baekjoon.backtracking;public class BOJ9663 {
+package baekjoon.backtracking;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ9663 {
+    static int N, cnt;
+    static boolean[] isused1, isused2, isused3;
+
+    public static void solve(int cur) {
+        if (cur == N) {
+            cnt++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (isused1[i] || isused2[cur + i] || isused3[cur - i + N - 1])
+                continue;
+            isused1[i] = true;
+            isused2[cur + i] = true;
+            isused3[cur - i + N - 1] = true;
+            solve(cur + 1);
+            isused1[i] = false;
+            isused2[cur + i] = false;
+            isused3[cur - i + N - 1] = false;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        solve(0);
+        System.out.print(cnt);
+
+        br.close();
+    }
 }
